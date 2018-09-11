5a0f04dae63f83840146a42cc75aa3f0d208750d
naranhoe <j.naranjojr@gmail.com>
2018-09-11 17:00:29 -0400

    add styling to both the edit and new forms

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -2,5 +2,5 @@ class Workout < ApplicationRecord
   belongs_to :user
   has_many :excercises, dependent: :destroy
 
-  accepts_nested_attributes_for: :excercises, allow_destroy: :true
+  accepts_nested_attributes_for :excercises, allow_destroy: true
 end
